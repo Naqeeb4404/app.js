b0d743b889c8eeba31a33fd3957864fe58f826b5
Naqeeb <naqeeblat@gmail.com>
2022-12-03 17:27:02 +0800

    26

---
+++ apps.js
@@ -0,0 +1,21 @@
+const { readFileSync, writeFileSync, readFile } = require('fs')
+
+readFile('./content/first.txt', 'utf8', (err, result) ==>{
+    if (err) {
+        console.log(err)
+        return
+    }
+    const seconds = result;
+    readFile('./content/secondtxt', 'utf8', (err,result)==>;
+        'Here is the result : ${first}, {second}`,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+                return
+            }
+            console first = result;
+            readFile('./content/second.txt',utf8')
+        }
+    )
+})
+})
